350bc8b56033547711bd6f5fcb294f49f947dadd
agung atidhira <agungatidhira@gmail.com>
2022-12-12 15:05:35 +0700

    add Mysql DAG

--- airflow/dags/dag_dim_tables.py
+++ airflow/dags/dag_dim_tables.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 # from airflow.providers.ssh.operators.ssh import SSHOperator
 
 default_args = {
@@ -30,7 +31,24 @@ with DAG(
     )
 
     # Dependency Mysql ETL DAG
+    csv_to_mysql = SparkSubmitOperator(
+        task_id = 'csv_to_mysql',
+        application = '/usr/local/spark/app/csv_to_mysql.py',
+        name='spark_mysql_etl1',
+        conn_id='spark_default',
+        jars='/opt/bitnami/spark/jars/mysql-connector-java-8.0.30.jar',
+        dag=dag
+    )
+    mysql_to_postgres = SparkSubmitOperator(
+        task_id = 'mysql_to_postgres',
+        application = '/usr/local/spark/app/mysql_to_postgres.py',
+        name='spark_mysql_etl2',
+        conn_id='spark_default',
+        jars='/opt/bitnami/spark/jars/mysql-connector-java-8.0.30.jar, /opt/bitnami/spark/jars/postgresql-42.2.25.jar',
+        dag=dag
+    )
 
+    # Dim Tables
     dim_country_state = BashOperator(
         task_id="dim_country_state",
         bash_command="python3 /opt/airflow/scripts/dim_state_n_country.py",
